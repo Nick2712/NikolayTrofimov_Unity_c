b218ede9821a062c1738834e77be68174b415232
Nick2712 <53843441+Nick2712@users.noreply.github.com>
2022-06-19 17:15:36 +0300

    убрал лишнее

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -12,40 +12,11 @@ namespace NikolayTrofimovUnityC
         private PlayerBall _player;
         private InteractiveObject[] _interactiveObjects;
         
+
         private void Start()
         {
             _player = FindObjectOfType<PlayerBall>();
             _interactiveObjects = FindObjectsOfType<InteractiveObject>();
-
-            //var objects = new ListIntractiveObject();
-            //foreach(var obj in objects)
-            //{
-            //    Debug.Log(obj);
-            //}
-
-            //SomeComparable[] tests = new SomeComparable[10];
-            //for(int i = 0; i < tests.Length; i++)
-            //{
-            //    tests[i].Value = UnityEngine.Random.Range(0, 100);
-            //}
-            //foreach(var test in tests)
-            //{
-            //    Debug.Log(test.Value);
-            //}
-            //Debug.Log("---");
-            //Array.Sort(tests);
-            //foreach (var test in tests)
-            //{
-            //    Debug.Log(test.Value);
-            //}
-            //Debug.Log("---");
-            //var comparer = new SomeComparableComparer();
-            //List<SomeComparable> tests2 = tests.ToList();
-            //tests2.Sort(comparer);
-            //foreach (var test2 in tests2)
-            //{
-            //    Debug.Log(test2.Value);
-            //}
         }
 
         private void FixedUpdate()
